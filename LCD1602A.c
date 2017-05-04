99ba2d6589c752c75b6216a1b12ba0e7d8236132
Abdel Alkuor <alkuor@gmail.com>
2017-05-04 14:25:37 -0400

    Update LCD1602A.c

--- LCD1602A.c
+++ LCD1602A.c
@@ -4,7 +4,7 @@
 * PROGRAMMERS	 	 : Abdel Alkuor
 * SECOND VERSION  	 : 2016-03-13
 * Description    	 : This library can be used to drive any 16x2 LCD
-			           it uses I2C protocol. The pins are connected as folloing for STM32F3 discovery board:
+			           it uses I2C1 protocol. The pins are connected as following for STM32F3 discovery board:
 		        	   SCL--->PA15
 			           SDA--->PA14
 						
@@ -14,14 +14,20 @@
 uint8_t buffer[1]={0};
 uint8_t portLCD=0;
 
-
+/* This fucntion is used to send a byte to the LCD whether data or command
+* @param: bt: byte to be sent
+*/
 void LCD_WriteByteI2CLCD(uint8_t bt)
 {
 	buffer[0] = bt;
-	HAL_I2C_Master_Transmit(&hi2c2,(uint16_t)0x4E, buffer,1,100);
+	HAL_I2C_Master_Transmit(&hi2c2,(uint16_t)0x4E, buffer,1,100); // 0x4E is the address of the I2C expansion IC you might need to
+                                                                  // change it to the address of the I2C expansion IC you are using
+                                                                  // consult the datasheet of the IC you are using
 }
 
-
+/* This fucntion is used to send half a byte (a command) to the LCD 
+* @param: c: command to be sent
+*/
 void sendHalfByte(uint8_t c)
 {
 	c<<=4;
@@ -32,6 +38,9 @@ void sendHalfByte(uint8_t c)
 	HAL_Delay(1);
 }
 
+/* This fucntion is used to send full byte to the LCD whether data or command
+* @param: c: byte to be sent
+*/
 void sendByte(uint8_t c, uint8_t mode)
 {
 	uint8_t Msbits = 0;
@@ -42,6 +51,7 @@ void sendByte(uint8_t c, uint8_t mode)
 
 }
 
+/* This function can be used to clear the whole LCD display */
 void clearLCD(void)
 {
 	sendByte(LCD1602A_CLEAR_DISPLAY, 0);
@@ -76,11 +86,11 @@ void LCD1602A_init(uint8_t numberOfLine)
 	
 	if (numberOfLine>1)
 	{
-		sendByte( LCD1602A_FUCNTION_SET | LCD1602A_2LINE_DISPLAY | LCD1602A_5x8DOTS_FORMAT, 0 );
+		sendByte( LCD1602A_FUCNTION_SET | LCD1602A_2LINE_DISPLAY | LCD1602A_5x8DOTS_FORMAT, 0);
 	}
 	else
 	{
-		sendByte( LCD1602A_FUCNTION_SET | LCD1602A_1LINE_DISPLAY | LCD1602A_5x8DOTS_FORMAT, 0 );
+		sendByte( LCD1602A_FUCNTION_SET | LCD1602A_1LINE_DISPLAY | LCD1602A_5x8DOTS_FORMAT, 0);
 	}
 	
 	sendByte( LCD1602A_DISPLAY_CONTROL | LCD1602A_DISPLAY_ON | LCD1602A_CURSOR_ON | LCD1602A_CURSOR_BLINK_ON, 0);
@@ -96,7 +106,7 @@ void LCD1602A_init(uint8_t numberOfLine)
 /* this function is used to turn the display ON */
 void LCD1602A_DisplayOn(void)
 {
-	sendByte( LCD1602A_DISPLAY_CONTROL | LCD1602A_DISPLAY_ON, 0 );
+	sendByte( LCD1602A_DISPLAY_CONTROL | LCD1602A_DISPLAY_ON, 0);
 	HAL_Delay(10);
 }
 /* this function is used to turn the display OFF */
@@ -108,7 +118,7 @@ void LCD1602A_DisplayOff(void)
 /* this function is used to have blinking cursor ON */
 void LCD1602A_BlinkOn(void)
 {
-	sendByte( LCD1602A_DISPLAY_CONTROL | LCD1602A_CURSOR_BLINK_ON, 0 );
+	sendByte( LCD1602A_DISPLAY_CONTROL | LCD1602A_CURSOR_BLINK_ON, 0);
 	HAL_Delay(10);
 }
 
@@ -163,7 +173,7 @@ void LCD1602A_ShiftCursorRight(void)
 /* This function is used to return the cursor at the beginning of the display*/
 void LCD1602A_ReturnHome (void)
 {
-	sendByte( LCD1602A_RETURN_HOME, 0 );
+	sendByte( LCD1602A_RETURN_HOME, 0);
 	HAL_Delay(10);
 }
 
@@ -173,7 +183,7 @@ void LCD1602A_ReturnHome (void)
 */
 void LCD1602A_WriteLineOne(uint8_t col, char *str)
 {	
-	sendByte( LCD1602A_SET_DDRAM_ADDRESS | col | 0x00, 0 );// 0x00 is the address of the first column in the first row
+	sendByte( LCD1602A_SET_DDRAM_ADDRESS | col | 0x00, 0);// 0x00 is the address of the first column in the first row of The LCD
 	
 	while(*str)
 	{
@@ -189,7 +199,7 @@ void LCD1602A_WriteLineOne(uint8_t col, char *str)
 */
 void LCD1602A_WriteLineTwo(uint8_t col, char *str)
 {
-	sendByte( LCD1602A_SET_DDRAM_ADDRESS | col | 0x40, 0 );// 0x40 is the address of the first column in the second row
+	sendByte( LCD1602A_SET_DDRAM_ADDRESS | col | 0x40, 0 );// 0x40 is the address of the first column in the second row of the LCD
 	while(*str)
 	{
 		LCD1602A_Data( *str );
